42af06ebf37e2671c979755b9506bd2af367653a
Jordan <jmarkle@ksu.edu>
2017-04-20 15:36:21 -0500

    Request list view is filled

--- 677FinalProject/LoginForm.cs
+++ 677FinalProject/LoginForm.cs
@@ -94,5 +94,11 @@ namespace _677FinalProject
                 }
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RequestForm r = new RequestForm();
+            r.ShowDialog();
+        }
     }
 }
