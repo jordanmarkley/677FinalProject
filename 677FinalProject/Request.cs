a233e70c3dac90f6f833d49c08396cde52bea13b
Jordan <jmarkle@ksu.edu>
2017-04-25 15:04:19 -0500

    stuff

--- 677FinalProject/Request.cs
+++ 677FinalProject/Request.cs
@@ -8,94 +8,18 @@ namespace _677FinalProject
 {
     class Request
     {
-        //Variables
-        private int requestID;
-        private List<SoftwareHardware> requestList;
-        private decimal subtotalPrice;
+        private int reqID;
+        private int supID;
+        private int empID;
+        private int status;
+        private decimal subtotal;
         private decimal salesTax;
         private decimal totalPrice;
-        private Employee emp;
+        private string date;
 
-        //Blank constructor
-        public Request()
+        public Request(int sID, int eID, decimal sub, decimal tax, decimal price, string d)
         {
-
-        }
-
-        //Constructor - 5 parameters
-        public Request(int id, List<SoftwareHardware> list, decimal subtotal, decimal tax, decimal total)
-        {
-            requestID = id;
-            requestList = list;
-            subtotalPrice = subtotal;
-            salesTax = tax;
-            totalPrice = total;
-        }
-
-        //Getters and setters for variables
-        public int RequestID
-        {
-            get
-            {
-                return requestID;
-            }
-
-            set
-            {
-                requestID = value;
-            }
-        }
-
-        public List<SoftwareHardware> ItemList
-        {
-            get
-            {
-                return requestList;
-            }
-
-            set
-            {
-                requestList = value;
-            }
-        }
-
-        public decimal Subtotal
-        {
-            get
-            {
-                return subtotalPrice;
-            }
-
-            set
-            {
-                subtotalPrice = value;
-            }
-        }
-
-        public decimal SalesTax
-        {
-            get
-            {
-                return salesTax;
-            }
-
-            set
-            {
-                salesTax = value;
-            }
-        }
-
-        public decimal Total
-        {
-            get
-            {
-                return totalPrice;
-            }
-
-            set
-            {
-                totalPrice = value;
-            }
+            
         }
     }
 }
